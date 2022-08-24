18114673aacf2f31258af3ac6a5e5b6838f638d7
Juan camilo Torres Gonzalez <91229391+juancamilotorresgonzalez@users.noreply.github.com>
2022-08-24 11:41:11 -0500

    Upload refactor project

---
+++ karateproyectoelvis/target/test-classes/karate-config.js
@@ -0,0 +1,11 @@
+
+function fn(){
+    var env = karate.env;
+    var properties = read ('classpath:karate-properties.json')
+
+    if(!env){
+        env = 'dev'
+    }
+    var config = properties[env];
+    return config;
+}
